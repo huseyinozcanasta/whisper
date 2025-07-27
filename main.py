9e2f27fc2dc4a6967f293040b15fda7b4e0aa6e8
huseyinozcanasta <20220100134@ogr.akdeniz.edu.tr>
2025-07-27 17:27:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,5 +20,6 @@ def transcribe():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
